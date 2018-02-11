7ba620a51cd685116d48991d80cec7316984f243
TaikiFnit <taikifnitspg@gmail.com>
2018-02-12 03:37:35 +0900

    introdouce tab and list view

--- App.js
+++ App.js
@@ -1,21 +1,118 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Icon,
+  Text
+} from 'native-base';
+import {TabNavigator} from "react-navigation";
+import PlayersScreen from './PlayersScreen'
+import PlayersListThumbnail from './PlayersListThumbnail';
 
-export default class App extends React.Component {
-  render() {
+import firebase from 'firebase';
+import firebaseConfig from './firebase-config';
+
+export default(MainScreenNavigator = TabNavigator({
+  Players: {
+    screen: props => <PlayersScreen {...props}/>
+  },
+  Chat: {
+    screen: props => <PlayersListThumbnail {...props}/>
+  },
+  Settings: {
+    screen: props => <PlayersListThumbnail {...props}/>
+  }
+}, {
+  tabBarPosition: "bottom",
+  tabBarComponent: props => {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Footer>
+        <FooterTab>
+          <Button
+            vertical
+            active={props.navigationState.index === 0}
+            onPress={() => props.navigation.navigate("Players")}>
+            <Icon name="bowtie"/>
+            <Text>Players</Text>
+          </Button>
+          <Button
+            vertical
+            active={props.navigationState.index === 1}
+            onPress={() => props.navigation.navigate("Chat")}>
+            <Icon name="briefcase"/>
+            <Text>Chat</Text>
+          </Button>
+          <Button
+            vertical
+            active={props.navigationState.index === 2}
+            onPress={() => props.navigation.navigate("Settings")}>
+            <Icon name="headset"/>
+            <Text>Settings</Text>
+          </Button>
+        </FooterTab>
+      </Footer>
+    );
+  }
+}));
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 1,
+      players: {}
+    }
+  }
+  componentDidMount() {
+    firebase.initializeApp(firebaseConfig);
+    this.usersRef = firebase
+      .database()
+      .ref('users');
+    this
+      .usersRef
+      .on('value', (snapshot) => {
+        this.setState({
+          players: snapshot.val()
+        });
+      });
+  }
+  getFragment() {}
+  render()
+  {
+    return (
+      <Container>
+        <Header/>
+        <Content></Content>
+        <Footer>
+          <FooterTab>
+            <Button
+              vertical
+              active={props.navigationState.index === 0}
+              onPress={() => props.navigation.navigate("LucyChat")}>
+              <Icon name="bowtie"/>
+              <Text>Lucy</Text>
+            </Button>
+            <Button
+              vertical
+              active={props.navigationState.index === 1}
+              onPress={() => props.navigation.navigate("JadeChat")}>
+              <Icon name="briefcase"/>
+              <Text>Nine</Text>
+            </Button>
+            <Button
+              vertical
+              active={props.navigationState.index === 2}
+              onPress={() => props.navigation.navigate("NineChat")}>
+              <Icon name="headset"/>
+              <Text>Jade</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ PlayersListThumbnail.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  Body,
+  Button,
+  Left,
+  Right,
+  Icon
+} from 'native-base';
+import {unixTime2ymd} from './TimerHelpers';
+
+export default class PlayersListThumbnail extends React.Component {
+
+  render() {
+    return (
+      <List
+        dataArray={this.props.players}
+        renderRow={(player) => <ListItem>
+        <Thumbnail
+          size={80}
+          source={{
+          uri: "https://minotar.net/avatar/" + player.name
+        }}/>
+        <Body>
+          <Text>{player.name}</Text>
+          <Text note>{player.isOnline
+              ? "Online"
+              : "Last Login: " + unixTime2ymd(player.lastLogin)}</Text>
+        </Body>
+        <Right>
+          <Button transparent info>
+            <Icon name="ios-arrow-forward-outline"/>
+          </Button>
+        </Right>
+      </ListItem>}></List>
+    );
+  }
+}
