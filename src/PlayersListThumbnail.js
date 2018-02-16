14427e8fecea5a539053dd47c5412d81382103fe
TaikiFnit <taikifnitspg@gmail.com>
2018-02-16 17:17:21 +0900

    change FireStore from Firebase Realtime DB

--- src/PlayersListThumbnail.js
+++ src/PlayersListThumbnail.js
@@ -11,34 +11,40 @@ import {
   Button,
   Left,
   Right,
-  Icon
+  Icon,
 } from 'native-base';
 import {unixTime2ymd} from './TimerHelpers';
 
 export default class PlayersListThumbnail extends React.Component {
 
   render() {
+
     return (
       <List
         dataArray={this.props.players}
-        renderRow={(player) => <ListItem>
-        <Thumbnail
-          size={80}
-          source={{
-          uri: "https://minotar.net/avatar/" + player.name
-        }}/>
-        <Body>
-          <Text>{player.name}</Text>
-          <Text note>{player.isOnline
-              ? "Online"
-              : "Last Login: " + unixTime2ymd(player.lastLogin)}</Text>
-        </Body>
-        <Right>
-          <Button transparent info>
-            <Icon name="ios-arrow-forward-outline"/>
-          </Button>
-        </Right>
-      </ListItem>}></List>
+        renderRow={(player) => {
+
+          return (
+            <ListItem>
+              <Thumbnail
+                size={80}
+                source={{
+                  uri: "https://minotar.net/avatar/" + player.name
+                }} />
+              <Body>
+                <Text>{player.name}</Text>
+                <Text note>{player.isOnline
+                  ? "Online"
+                  : "Last Login: " + unixTime2ymd(player.lastLogin)}</Text>
+              </Body>
+              <Right>
+                <Button transparent info>
+                  <Icon name="ios-arrow-forward-outline" />
+                </Button>
+              </Right>
+            </ListItem>
+          );
+        }}></List>
     );
   }
 }
